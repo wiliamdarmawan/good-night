56b012c74670340e3f129c671cd945aecbfdac37
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:18:01 +0700

    add user feed api 404 error test case

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -25,6 +25,26 @@ RSpec.describe 'users', type: :request do
       }
 
       let(:pagination_params) { {} }
+
+      response(404, 'Record not found') do
+        context 'when user does not exist' do
+          let(:user_id) { 'foo' }
+
+          run_test! do
+            expect(response_body).to match(
+              {
+                errors: [
+                  {
+                    error: 'User not found',
+                    errorCode: 'GN-2',
+                    errorHandling: "Please ensure you've entered the correct parameters"
+                  }
+                ]
+              }
+            )
+          end
+        end
+      end
     end
   end
 end
