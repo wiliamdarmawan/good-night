ce525b373dd75142ed6bf1b351d600c7be3257c8
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:10:34 +0700

    add following api params validation test case

--- spec/controllers/follows_controller_spec.rb
+++ spec/controllers/follows_controller_spec.rb
@@ -40,6 +40,88 @@ RSpec.describe 'follows', type: :request do
           }
         }
       end
+
+      response(400, 'Invalid params') do
+        context 'when given non camelCase params' do
+          let(:params) do
+            {
+              data: {
+                attributes: {
+                  following_id: user_to_follow.id
+                }
+              }
+            }
+          end
+
+          run_test! do
+            expect(response_body).to match(
+              {
+                errors: [
+                  {
+                    error: 'Params following_id must be in camelCase(followingId)',
+                    errorCode: 'GN-1',
+                    errorHandling: "Please ensure you've entered the correct parameters"
+                  }
+                ]
+              }
+            )
+          end
+        end
+
+        context 'when followingId is not present' do
+          before { params[:data][:attributes][:followingId] = nil }
+
+          run_test! do
+            expect(response_body).to match(
+              {
+                errors: [
+                  {
+                    error: 'Following ID cannot be null',
+                    errorCode: 'GN-1',
+                    errorHandling: "Please ensure you've entered the correct parameters"
+                  }
+                ]
+              }
+            )
+          end
+        end
+
+        context 'when user tries to follow themselves' do
+          let(:user_to_follow) { user }
+
+          run_test! do
+            expect(response_body).to match(
+              {
+                errors: [
+                  {
+                    error: 'User cannot follow themselves',
+                    errorCode: 'GN-1',
+                    errorHandling: "Please ensure you've entered the correct parameters"
+                  }
+                ]
+              }
+            )
+          end
+        end
+
+        context 'when user has already follow the user to be followed' do
+          before { user.follow(user_to_follow) }
+
+          run_test! do
+            expect(response_body).to match(
+              {
+                errors: [
+                  {
+                    error: 'User has been followed before',
+                    errorCode: 'GN-1',
+                    errorHandling: "Please ensure you've entered the correct parameters"
+                  }
+                ]
+              }
+            )
+          end
+        end
+      end
     end
   end
 end
