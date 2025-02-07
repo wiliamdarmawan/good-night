657bb87993d3eded68429860ad776ec1a55eae28
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 20:50:33 +0700

    set up routes for the apis

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  resources :users, only: %i[show] do
+    resources :followings, only: %i[create destroy], controller: 'follows'
+    resources :clock_ins, path: 'clock-ins', only: %i[index], controller: 'clock_ins' do
+      collection do
+        post 'wake', to: 'clock_ins#wake'
+        post 'sleep', to: 'clock_ins#sleep'
+      end
+    end
+  end
 end
