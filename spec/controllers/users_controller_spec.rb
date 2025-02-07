6a49455058eb7034e7dc3b092d8a96bcb4c3ac1a
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:20:15 +0700

    add followings with sleep records & pagination params size test case

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -162,6 +162,35 @@ RSpec.describe 'users', type: :request do
             )
           end
         end
+
+        context "when users's followings has sleep records & pagination params size given" do
+          before { (1..25).map { |i| create(:sleep_record, user: followed_user, sleep_time: i.minutes.ago, wake_time: Time.now) } }
+
+          let(:pagination_params) do
+            {
+              page: {
+                size: 30,
+              }
+            }
+          end
+
+          run_test! do
+            expect(response_body[:data].count).to eq(25)
+            expect(response_body[:data].first[:attributes][:duration].to_i).to be > response_body[:data].second[:attributes][:duration].to_i
+            expect(response_body[:data]).to all(include(:id, :type, :attributes, :relationships))
+            expect(response_body[:meta]).to match(
+              {
+                total: 25,
+                pages: 1
+              }
+            )
+            expect(response_body[:links]).to match(
+              {
+                self: %r{\Ahttp?://[^/]+/users/#{user_id}\?page\[size\]=30\z},
+              }
+            )
+          end
+        end
       end
     end
   end
