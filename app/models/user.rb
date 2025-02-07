e5316abe61277a9ab682eafe52ee1d3ba2839502
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 20:17:02 +0700

    add follow functions to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,18 @@ class User < ApplicationRecord
 
   has_many :followers, through: :follows_received, source: :follower
   has_many :followings_sleep_records, through: :followings, source: :sleep_records
+
+  def follow(user)
+    raise InvalidParamsError, 'User cannot follow themselves' if self == user
+
+    follows_given.create!(following: user)
+  end
+
+  def unfollow(user)
+    followings.delete(user)
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
