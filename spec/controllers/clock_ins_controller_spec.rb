d9a6cf7c1c67a311ed90d636cbcf508b3f176fce
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:07:59 +0700

    create clock in wake 404 error test case

--- spec/controllers/clock_ins_controller_spec.rb
+++ spec/controllers/clock_ins_controller_spec.rb
@@ -333,6 +333,26 @@ RSpec.describe 'clock-ins', type: :request do
           end
         end
       end
+
+      response(404, 'Record not found') do
+        context 'when user does not exist' do
+          let(:user_id) { 'foo' }
+
+          run_test! do
+            expect(response_body).to match(
+              {
+                errors: [
+                  {
+                    error: 'User not found',
+                    errorCode: 'GN-2',
+                    errorHandling: "Please ensure you've entered the correct parameters"
+                  }
+                ]
+              }
+            )
+          end
+        end
+      end
     end
   end
 end
