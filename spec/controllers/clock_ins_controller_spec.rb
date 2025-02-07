edbdaf6908f35dbbba4b5b63de0d5bd5f5c18d7a
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:01:59 +0700

    add clock in with no pagination params test case

--- spec/controllers/clock_ins_controller_spec.rb
+++ spec/controllers/clock_ins_controller_spec.rb
@@ -63,6 +63,29 @@ RSpec.describe 'clock-ins', type: :request do
             )
           end
         end
+
+        context 'when clock ins log exists & no pagination params given' do
+          before { (1..15).map { |i| create(:clock_in, user: user, created_at: Time.now - i.hours) } }
+
+          run_test! do
+            expect(response_body[:data].count).to eq(10)
+            expect(Time.parse(response_body[:data].first[:attributes][:createdAt])).to be > Time.parse(response_body[:data].second[:attributes][:createdAt])
+            expect(response_body[:data]).to all(include(:id, :type, :attributes, :relationships))
+            expect(response_body[:meta]).to match(
+              {
+                total: 15,
+                pages: 2
+              }
+            )
+            expect(response_body[:links]).to match(
+              {
+                self: %r{\Ahttp?://[^/]+/users/#{user_id}/clock-ins\?page\[size\]=10\z},
+                next: %r{\Ahttp?://[^/]+/users/#{user_id}/clock-ins\?page\[number\]=2&page\[size\]=10\z},
+                last: %r{\Ahttp?://[^/]+/users/#{user_id}/clock-ins\?page\[number\]=2&page\[size\]=10\z}
+              }
+            )
+          end
+        end
       end
     end
   end
