09fbd7cb6469064c519f7027f94263fd0e008dcd
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:11:56 +0700

    create unfollow api handler

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -16,4 +16,19 @@ class FollowsController < ApplicationController
 
     render json: FollowSerializer.new(following).serializable_hash.to_json, status: :created
   end
+
+  def destroy
+    user_id = parsed_params[:user_id]
+    user = GetUserService.call(user_id)
+    user_to_unfollow_id = parsed_params[:id]
+
+    raise InvalidParamsError, 'User cannot unfollow themselves' if user_id.to_s == user_to_unfollow_id.to_s
+
+    user_to_unfollow = GetUserService.call(user_to_unfollow_id, error_message: 'User to unfollow not found')
+    raise InvalidParamsError, "User hasn't been followed before" unless user.following?(user_to_unfollow)
+
+    user.unfollow(user_to_unfollow)
+
+    render json: {}, status: :no_content
+  end
 end
