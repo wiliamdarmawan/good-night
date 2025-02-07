9a14e370a12effbd4c143aae4d0f78609ebcfc1e
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:04:14 +0700

    create clock in sleep api handler

--- app/controllers/clock_ins_controller.rb
+++ app/controllers/clock_ins_controller.rb
@@ -13,4 +13,15 @@ class ClockInsController < ApplicationController
 
     render_paginated(clock_ins, pagination_params, serializer: ClockInSerializer)
   end
+
+  def sleep
+    user_id = parsed_params[:user_id]
+    user = GetUserService.call(user_id)
+
+    raise InvalidParamsError, 'User is sleeping' if user.clock_ins&.last&.sleep?
+
+    clock_in = user.sleep!
+
+    render json: ClockInSerializer.new(clock_in, { params: { show_relationships: true } }).serializable_hash.to_json, status: :created
+  end
 end
