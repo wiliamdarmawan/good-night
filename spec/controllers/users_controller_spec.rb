1b739e372393a1ddc50b413b761b66a9159e68b4
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 21:19:57 +0700

    add followings with sleep records & pagination params number test case

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -129,6 +129,39 @@ RSpec.describe 'users', type: :request do
             )
           end
         end
+
+        context "when users's followings has sleep records & pagination params number given" do
+          before { (1..25).map { |i| create(:sleep_record, user: followed_user, sleep_time: i.minutes.ago, wake_time: Time.now) } }
+
+          let(:pagination_params) do
+            {
+              page: {
+                number: 2
+              }
+            }
+          end
+
+          run_test! do
+            expect(response_body[:data].count).to eq(10)
+            expect(response_body[:data].first[:attributes][:duration].to_i).to be > response_body[:data].second[:attributes][:duration].to_i
+            expect(response_body[:data]).to all(include(:id, :type, :attributes, :relationships))
+            expect(response_body[:meta]).to match(
+              {
+                total: 25,
+                pages: 3
+              }
+            )
+            expect(response_body[:links]).to match(
+              {
+                first: %r{\Ahttp?://[^/]+/users/#{user_id}\?page\[size\]=10\z},
+                prev: %r{\Ahttp?://[^/]+/users/#{user_id}\?page\[size\]=10\z},
+                self: %r{\Ahttp?://[^/]+/users/#{user_id}\?page\[number\]=2&page\[size\]=10\z},
+                next: %r{\Ahttp?://[^/]+/users/#{user_id}\?page\[number\]=3&page\[size\]=10\z},
+                last: %r{\Ahttp?://[^/]+/users/#{user_id}\?page\[number\]=3&page\[size\]=10\z}
+              }
+            )
+          end
+        end
       end
     end
   end
