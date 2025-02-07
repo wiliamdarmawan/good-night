6bca00b3cc84f9deb3fa92406708d631c80e44b1
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 20:52:32 +0700

    create error handling on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include Paginable
+  rescue_from StandardError, with: :handle_error
+
+  private
+
+  def handle_error(error)
+    error_details = Constants::ERROR_MAPPINGS[error.class] || {
+      message: 'An unexpected error occurred',
+      code: 'GN-999',
+      handling: 'Please contact support',
+      status: :internal_server_error
+    }
+    error_message = error.message == error.class.to_s ? error_details[:message] : error.message
+
+    render json: {
+      errors: [
+        {
+          error: error_message,
+          errorCode: error_details[:code],
+          errorHandling: error_details[:handling]
+        }
+      ]
+    }, status: error_details[:status]
+  end
 end
