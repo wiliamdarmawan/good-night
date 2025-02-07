f6438ac88972fed8c1b5073bc3acbecb0fe12a7c
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-02-07 20:16:37 +0700

    add self reference relationship to follows table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@
 class User < ApplicationRecord
   has_many :clock_ins
   has_many :sleep_records
+
+  has_many :follows_given, class_name: 'Follow',
+                           foreign_key: 'follower_id',
+                           dependent: :destroy
+
+  has_many :followings, through: :follows_given, source: :following
+
+  has_many :follows_received, class_name: 'Follow',
+                              foreign_key: 'following_id',
+                              dependent: :destroy
+
+  has_many :followers, through: :follows_received, source: :follower
+  has_many :followings_sleep_records, through: :followings, source: :sleep_records
 end
